4cb80788bdbed42a4ec1b4377224041d2b4ea4ad
sguera <sguera@users.noreply.github.com>
2017-05-09 15:20:53 +0200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -4,7 +4,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-98787777-1', 'auto');
+  ga('create', 'UA-98787777-2', 'auto');
   ga('send', 'pageview');
 
 </script>
